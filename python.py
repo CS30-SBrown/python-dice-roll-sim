e64e8c97c249d018f5ddc51c7f7527d6a3e3b00a
mrpapus <srednalf05@icloud.com>
2023-06-12 17:23:39 -0600

    Update python.py

--- python.py
+++ python.py
@@ -1,30 +1,59 @@
 
-the_menu_list =("Roll Dice Once \n"
+the_menu_list =("\n""MAIN MENU\n""Roll Dice Once \n"
 "Roll Dice 5 Times\n"
 "Roll Dice ‘n’ Times\n"
 "Roll Dice until Snake Eyes\n"
 "Exit")
-
-import random
-print(the_menu_list
-)
-choice = input("Select an option (1-5):")
-
-if choice == "1":
-    print("works1")
-elif choice == "2":
-    print("works2")
-elif choice == "3":
-    print("works3")
-elif choice == "4":
-    print("works4")
-elif choice == "5":
-    print("works5")
-else:
+sum = 0
+def roll_one_dice():
+    
+    global sum
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     sum = dice1+dice2
-print(dice1,",",dice2 ,"sum:",sum)
+    print(dice1,",",dice2,"sum:",sum,)
+import random
+
+      
+def begin_loop():
+    global sum
+    print(the_menu_list)
+
+    choice = input("Select an option (1-5):")
+
+    if choice == "1":
+        roll_one_dice()
+        begin_loop()
+    elif choice == "2":
+        i = 0
+        while i < 5:
+            i +=1
+            roll_one_dice()
+        begin_loop()
+    elif choice == "3":
+        number_of_times = int(input("input the # of times you want to roll:"))
+        i = 0
+        while i < number_of_times:
+            i +=1
+            roll_one_dice()
+        begin_loop()
+    elif choice == "4":
+    
+        i = 0
+        while sum != 2:
+            i+= 1
+            roll_one_dice()
+        sum = 0  
+        print("you rolled",i,"times")
+        begin_loop()
+
+            
+    elif choice == "5":
+        print("task_ended")
+begin_loop()        
+   
+    
+
 
 
 
